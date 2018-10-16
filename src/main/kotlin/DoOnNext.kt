10fe9918f58c9648c1c63499bd68356e2355c3ec
makoto.kobayashi <makoto.kobayashi@jmty.jp>
2018-10-16 17:54:04 +0900

    add doOnNext with flatMap

--- src/main/kotlin/DoOnNext.kt
+++ src/main/kotlin/DoOnNext.kt
@@ -3,8 +3,9 @@ import io.reactivex.rxkotlin.subscribeBy
 
 class DoOnNext {
     fun run() {
-        example()
-        exampleFlatMap()
+        //example()
+        //exampleFlatMap()
+        exampleFlatMap2()
     }
 
     fun example() {
@@ -25,20 +26,48 @@ class DoOnNext {
     }
 
     fun exampleFlatMap() {
-        Observable.create<String> { emitter ->
-            emitter.onNext("1")
-            emitter.onNext("2")
-            emitter.onError(RuntimeException())
-            emitter.onNext("3")
-            emitter.onComplete()
-        }.doOnNext {
-            println("doOnNext:$it")
-        }.flatMap {
-            Observable.just("rererere")
-        }.subscribeBy(
-                onError = { println("onError$it") },
-                onNext = { println("onNext$it") },
-                onComplete = { println("onComplete") }
-        )
+        val list = listOf("One", "Two", "Three", RuntimeException(), "Five")
+        println("---FlatMap---")
+        Observable
+                .fromIterable(list)
+                .doOnNext {
+                    println("-doOnNext:$it")
+                }
+                .flatMap {
+                    Observable.create<Any> { emitter ->
+                        if (it is Throwable) emitter.onError(it) else emitter.onNext("*$it*")
+                    }
+                }
+                .subscribeBy(
+                        onError =
+                        { println("----onError$it") },
+                        onNext =
+                        { println("----onNext$it") },
+                        onComplete =
+                        { println("----onComplete") }
+                )
+    }
+
+
+    fun exampleFlatMap2() {
+        val list = listOf("One", "Two", "Three", RuntimeException(), "Five")
+        println("---FlatMap---")
+        Observable
+                .fromIterable(list)
+                .flatMap { ele ->
+                    Observable.create<Any> { emitter ->
+                        if (ele is Throwable) emitter.onError(ele) else emitter.onNext("*$ele*")
+                    }.doOnNext {
+                        println("-doOnNext:$ele")
+                    }
+                }
+                .subscribeBy(
+                        onError =
+                        { println("----onError$it") },
+                        onNext =
+                        { println("----onNext$it") },
+                        onComplete =
+                        { println("----onComplete") }
+                )
     }
 }
